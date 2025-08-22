cbbf6803570ba025631cea0869b278d89e359562
OrangemanZ7 <ricardo.laranjeira.74@gmail.com>
2025-08-21 21:14:17 -0400

    apiService.ts localhost

--- gestao-manutencao/frontend/src/api/apiService.ts
+++ gestao-manutencao/frontend/src/api/apiService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Cria uma instância do Axios com a URL base da nossa API
 const api = axios.create({
-  baseURL: 'http://192.168.18.21:5000/api', // A porta do nosso back-end
+  baseURL: 'http://localhost:5000/api', // A porta do nosso back-end mudar localhost para IP da máquina na rede
 });
 
 export default api;
