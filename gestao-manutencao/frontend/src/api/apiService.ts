bc3f370e7b510579ebb2792a4d372e72a3539c25
OrangemanZ7 <ricardo.laranjeira.74@gmail.com>
2025-12-23 15:58:19 -0400

    acesso api na rede local

--- gestao-manutencao/frontend/src/api/apiService.ts
+++ gestao-manutencao/frontend/src/api/apiService.ts
@@ -4,13 +4,13 @@ import axios from "axios";
 // Cria uma instância do Axios com a URL base da nossa API
 
 // Uso local
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // A porta do nosso back-end
-});
+//const api = axios.create({
+//  baseURL: 'http://localhost:5000/api', // A porta do nosso back-end
+//});
 
 // Uso na rede
-// const api = axios.create({
-//   baseURL: "http://192.168.18.196:5000/api", // A porta do nosso back-end mudar o IP da máquina na rede
-// });
+const api = axios.create({
+  baseURL: "http://192.168.18.21:5000/api", // A porta do nosso back-end mudar o IP da máquina na rede
+});
 
 export default api;
